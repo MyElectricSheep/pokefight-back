835d18c9e159e15e8961f2f6a73d729b3efe941a
Ben Paget <ben.paget@live.fr>
2020-06-07 12:18:31 +0100

    Added CORS and cleaned up the routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var pokeRouter = require('./routes/pokemon');
@@ -9,10 +10,12 @@ var pokeRouter = require('./routes/pokemon');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/pokemon', pokeRouter);
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 const pokeController = require('../controllers/pokeController')
 
-// Create a GET route on /pokemon which gives the complete list of pokemon from the JSON
-// Create a GET route on /pokemon/:id which gives only on pokemon from the JSON thanks to its id
-// Create a GET route on /pokemon/:id/:info(name|type|base) which gives only on pokemon from the JSON thanks to its id and retrieve only one information (name or type or base)
-
+// Gives only one pokemon from the JSON thanks to its id 
+// and retrieve only one information (name or type or base)
 router.get('/:id/:info', pokeController.find_one_and_filter);
 
+// Gives only one pokemon from the JSON thanks to its id
 router.get('/:id', pokeController.find_one);
 
+// Gives the complete list of pokemon from the JSON
 router.get('/', pokeController.find_all);
-
 
 module.exports = router;
