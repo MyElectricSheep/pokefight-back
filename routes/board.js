08973e9b65a7941501f23fa5e8d5c7efa01afc68
Ben Paget <ben.paget@live.fr>
2020-06-15 16:15:50 +0100

    Added authorization for game creation

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const authorizePlayer = require('../middlewares/playerAuth')
 const boardController = require('../controllers/boardController')
 
 router.get('/all', boardController.find_all);
 
 router.get('/ranking', boardController.get_ranking);
 
-router.post('/save', boardController.create_one)
+router.post('/save', authorizePlayer, boardController.create_one)
 
 module.exports = router;
